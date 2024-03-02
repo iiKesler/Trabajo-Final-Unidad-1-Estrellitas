3f68bf81bd8a5b90b21ed153ba92f5fb8bb18055
Kusseler <andresmartinezbedoya@gmail.com>
2024-03-02 15:59:15 -0500

    Update Trabajo_Final.ino
    Added a cycle to check for the password, and made it print how much time it was left, when it reaches 0, it exits the cycle

--- Trabajo_Final/Trabajo_Final.ino
+++ Trabajo_Final/Trabajo_Final.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-void defuse() {
+
 enum class Task1States {
     INIT,
     CONFIG,
@@ -11,7 +11,12 @@ static char password[5];
 static uint8_t dataCounter = 0;
 static char result[2];
 static uint8_t numbersPassword = 0; // Falso
+static uint32_t defuseTime = 5;
+constexpr static uint32_t INTERVAL = 1000;
+static uint32_t lastTime = 0;
 
+
+void defuse() {
 
     switch (task1State) {
         case Task1States::INIT: {
@@ -24,8 +29,6 @@ static uint8_t numbersPassword = 0; // Falso
         case  Task1States::CONFIG:
         {
           Serial.print("S para subir");
-
-            uint32_t defuseTime = 5;
 
             while (true) {
                 if (Serial.available()) {
@@ -53,12 +56,16 @@ static uint8_t numbersPassword = 0; // Falso
         case Task1States::WAIT_DATA: {
             if (Serial.available()) {
                 const char c = Serial.read();
-                if (c == 'C') {
+                do{
+
+                  lastTime = millis();
+
+                  if (c == 'C') {
                     dataCounter = 0;
                     numbersPassword = 1;
 
                     password[dataCounter++] = c;
-                    if (dataCounter == 4 && numbersPassword = 1) {
+                    if (dataCounter == 4 && numbersPassword == 1) {
                         password[dataCounter] = '\0'; // null terminate the password
                         if (strcmp(password, "1234") == 0) {
                             Serial.println("YOU SAVED THE WORLD");
@@ -70,10 +77,25 @@ static uint8_t numbersPassword = 0; // Falso
                         }
                         dataCounter = 0;
                     } 
+
+                    uint32_t currentTime = millis ();
+                    
+                    if ((currentTime - lastTime) >= INTERVAL) {
+                      
+                      lastTime = currentTime;
+                      defuseTime--;
+                      Serial.println("Tiempo restante");
+                      Serial.println(defuseTime);
+                    }  
+                  } while (defuseTime > 0)
+                } 
+                else {
+                    task1State = Task1States::CONFIG;
                 }
+            }
             break;
-          }
         }
+
         default: {
             break;
         }
