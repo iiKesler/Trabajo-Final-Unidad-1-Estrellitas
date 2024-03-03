eaf77af3f6fa531ffdbcf000a0bcba305ee99202
Kusseler <andresmartinezbedoya@gmail.com>
2024-03-03 16:06:25 -0500

    Update Trabajo_Final.ino
    Updated the code with a lot of changes, mainly on the reading of the password and making it more clear and organized

--- Trabajo_Final/Trabajo_Final.ino
+++ Trabajo_Final/Trabajo_Final.ino
@@ -9,79 +9,105 @@ enum class Task1States {
 static auto task1State = Task1States::INIT;
 static char password[5];
 static uint8_t dataCounter = 0;
-static uint8_t numbersPassword = 0; // Falso
 static uint32_t defuseTime = 5;
 static constexpr uint32_t INTERVAL = 1000;
+static constexpr uint32_t INTERVAL2 = 2000;
 static uint32_t lastTime = 0;
+static uint8_t result[2];
+
+static void compareData(const char *pass, uint8_t size, uint8_t *res);
 
 void defuse() {
-
-  switch (task1State) {
-    case Task1States::INIT: {
+  switch (task1State){
+    case Task1States::INIT:{
       Serial.begin(115200);
       task1State = Task1States::CONFIG;
       break;
     }
-    case Task1States::CONFIG: {
-      Serial.println("S para subir");
-
-      while (true) {
-        if (Serial.available()) {
-          const char c = Serial.read();
-          if (c == 'L') {
-            Serial.println("Ahora sigue la cuenta regresiva");
-            task1State = Task1States::WAIT_DATA;
-            break;
-          }
-
-          if (c == 'S') {
-            defuseTime++;
-            Serial.println("Subio ");
-          }
-
-          if (c == 'B') {
-            defuseTime--;
-            Serial.println("Bajo ");
-          }
-            Serial.println(defuseTime);
+    case Task1States::CONFIG:{
+      defuseTime=5;
+      Serial.println("S para subir tiempo, B para bajar tiempo, L para iniciar la cuenta regresiva");
+      while (Serial.available()){
+        const char c = Serial.read();
+        if (c == 'L'){
+          Serial.println("Ahora sigue la cuenta regresiva ");
+          task1State = Task1States::WAIT_DATA;
+          break;
         }
-      }
-    break;
-    }
-
-    case Task1States::WAIT_DATA: {
-      do {
-        uint32_t currentTime = millis();
-
-        if ((currentTime - lastTime) >= INTERVAL) {
-          lastTime = currentTime;
+        if (c == 'S' && defuseTime < 40){
+          defuseTime++;
+          Serial.println("Subio ");
+        }
+        if (c == 'B' && defuseTime > 1){
           defuseTime--;
-          Serial.println("Tiempo restante");
-          Serial.println(defuseTime);
+          Serial.println("Bajo ");
         }
-
-        if (Serial.available()) {
-          password[dataCounter] = Serial.read();
-
-          if (strcmp(password, "C1234") == 0) {
-            Serial.println("YOU SAVED THE WORLD");
-            task1State = Task1States::CONFIG;
-
-            dataCounter = 0;
-          }
-        } 
-        else {
-          Serial.println("Contraseña incorrecta");
-          task1State = Task1States::CONFIG;
-        }
-      } while (defuseTime > 0);
+        Serial.println(defuseTime);
+      }
       break;
     }
+    case Task1States::WAIT_DATA:{
+      do{
+        if (Serial.available()>0){
+          password[dataCounter]=Serial.read();
+          dataCounter++;
+          if(dataCounter==5){
+            compareData(password, dataCounter,result);
+            dataCounter=0;
+            Serial.print("result: ");
+            Serial.print(result[1]);
+            if(result[1]==1){
+              Serial.print("Salvaste el mundo ");
+              task1State = Task1States::CONFIG;
+              break;
+            }
+            else{
+              Serial.print("Respuesta incorrecta ");
+              task1State = Task1States::WAIT_DATA;
+              break;
+            }
+          }
+        }
+        const uint32_t currentTime = millis();
+        if ((currentTime - lastTime) >= INTERVAL) {
+          Serial.println("Tiempo restante ");
+          lastTime = currentTime;
+          defuseTime--;
+          Serial.println(defuseTime);
+        }
+      } while (defuseTime > 0 );
 
+      if(defuseTime==0){
+        Serial.print("RADIACIÓN NUCLEAR ACTIVA ");
+        const uint32_t currentTime2 = millis();
+        do{
+          if ((currentTime2 - lastTime) >= INTERVAL2){
+            task1State = Task1States::CONFIG;
+            break;
+          }
+        }while(defuseTime != 0);
+      }
+      break;
+    }
     default: {
       break;
     }
   }
+}
+
+static void compareData(const char *pass, const uint8_t size, uint8_t *res){
+  constexpr char compare[5]={'C','1','2','3','4'};
+  bool same=true;
+
+  for(int i=0;i<size;i++){
+    if (pass[i]!=compare[i]){
+      same=false;
+      break;
+    }
+  }
+
+  res[0] = same ? 0 : 1;
+  res[1] = same ? 1 : 0;
 }
 
 void setup() {
