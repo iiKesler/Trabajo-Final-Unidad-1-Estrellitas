287baa760b8920baed8b24ef86bc0b00ee893632
Andrés_Martínez <andres.martinezb@upb.edu.co>
2024-03-03 19:04:16 -0500

    Update Trabajo_Final.ino
    Updated the final project with updated messages, added a delay between switching the states of the programs

--- Trabajo_Final/Trabajo_Final.ino
+++ Trabajo_Final/Trabajo_Final.ino
@@ -15,100 +15,122 @@ static constexpr uint32_t INTERVAL2 = 2000;
 static uint32_t lastTime = 0;
 static uint8_t result[2];
 
-static void compareData(const char *pass, uint8_t size, uint8_t *res);
-
 void defuse() {
-  switch (task1State){
-    case Task1States::INIT:{
+  switch (task1State) {
+    case Task1States::INIT: {
       Serial.begin(115200);
+      delay(2000);
+      Serial.print("AHORA ENTRANDO AL MODO CONFIGURACIÓN");
+      delay(2000);
       task1State = Task1States::CONFIG;
       break;
     }
-    case Task1States::CONFIG:{
-      defuseTime=5;
-      Serial.println("S para subir tiempo, B para bajar tiempo, L para iniciar la cuenta regresiva");
-      while (Serial.available()){
-        const char c = Serial.read();
-        if (c == 'L'){
-          Serial.println("Ahora sigue la cuenta regresiva ");
-          task1State = Task1States::WAIT_DATA;
-          break;
+    case Task1States::CONFIG: {
+      defuseTime = 5;
+      Serial.println("'S' para aumentar 1 segundo, 'B' para bajar 1 segundo, 'L' para salir de la configuración. ");
+      Serial.println("Segundos por defecto: 5");
+      while (true) {
+        if (Serial.available()) {
+          const char c = Serial.read();
+          if (c == 'L') {
+            Serial.println("Ahora Inicia la cuenta regresiva ");
+            delay(2000);
+            task1State = Task1States::WAIT_DATA;
+            break;
+          }
+          if (c == 'S') {
+            if(defuseTime == 40){
+              Serial.println("No se pueden agregar más de 40 segundos ");
+            }
+            else{
+              defuseTime++;
+              Serial.println("Subió 1 segundo ");
+            }
+          }
+          if (c == 'B') {
+            if(defuseTime == 1){
+              Serial.println("No se puede bajar a menos de 1 segundo ");
+            }
+            else{
+              defuseTime--;
+              Serial.println("Bajó 1 segundo ");;
+            }             
+          }
+          Serial.println(defuseTime);
         }
-        if (c == 'S' && defuseTime < 40){
-          defuseTime++;
-          Serial.println("Subio ");
-        }
-        if (c == 'B' && defuseTime > 1){
-          defuseTime--;
-          Serial.println("Bajo ");
-        }
-        Serial.println(defuseTime);
       }
       break;
     }
-    case Task1States::WAIT_DATA:{
-      do{
+    case Task1States::WAIT_DATA: {
+      do {                    
         if (Serial.available()>0){
-          password[dataCounter]=Serial.read();
+          password[dataCounter] = Serial.read();
           dataCounter++;
-          if(dataCounter==5){
+          if(dataCounter == 5){
             compareData(password, dataCounter,result);
             dataCounter=0;
-            Serial.print("result: ");
-            Serial.print(result[1]);
-            if(result[1]==1){
-              Serial.print("Salvaste el mundo ");
+            Serial.println("result: ");
+            Serial.println(result[1]);
+            if(result[1] == 1){
+              Serial.println("Salvaste el mundo");
+              delay(2000);
               task1State = Task1States::CONFIG;
               break;
             }
             else{
-              Serial.print("Respuesta incorrecta ");
+              Serial.println("Respuesta incorrecta");
               task1State = Task1States::WAIT_DATA;
               break;
-            }
+            }          
           }
         }
-        const uint32_t currentTime = millis();
-        if ((currentTime - lastTime) >= INTERVAL) {
-          Serial.println("Tiempo restante ");
-          lastTime = currentTime;
-          defuseTime--;
-          Serial.println(defuseTime);
-        }
-      } while (defuseTime > 0 );
-
-      if(defuseTime==0){
-        Serial.print("RADIACIÓN NUCLEAR ACTIVA ");
-        const uint32_t currentTime2 = millis();
-        do{
-          if ((currentTime2 - lastTime) >= INTERVAL2){
+      uint32_t currentTime = millis();
+      if ((currentTime - lastTime) >= INTERVAL) {
+        Serial.println("Tiempo restante");
+        lastTime = currentTime;
+        defuseTime--;                            
+        Serial.println(defuseTime);
+      }
+    }while (defuseTime > 0 );
+      if(defuseTime == 0){
+        Serial.println("RADIACIÓN NUCLEAR ACTIVA");            
+        uint32_t currentTime2 = millis();
+        bool is = true;
+        do{    
+          if ((currentTime2 - lastTime) >= INTERVAL2) {
+            is = false;
+            delay(2000);
             task1State = Task1States::CONFIG;
             break;
-          }
-        }while(defuseTime != 0);
+          }  
+        }while(is == true);          
       }
       break;
     }
+
     default: {
       break;
     }
   }
 }
-
 static void compareData(const char *pass, const uint8_t size, uint8_t *res){
-  constexpr char compare[5]={'C','1','2','3','4'};
-  bool same=true;
+  constexpr char compare[5] = {'C','1','2','3','4'};
+  bool same = true;
 
-  for(int i=0;i<size;i++){
-    if (pass[i]!=compare[i]){
-      same=false;
-      break;
+  for(int i = 0; i < size; i++){
+    if (pass[i] == compare[i]){
+      same = true;
     }
+    else{
+      same = false;
+      i = 5;      
+    }  
   }
 
   res[0] = same ? 0 : 1;
   res[1] = same ? 1 : 0;
 }
+
 
 void setup() {
   defuse();
